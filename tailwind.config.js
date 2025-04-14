a35ba934704333d05302fdf4b0e50042cae315cb
sevycakes <yvescornejo21@gmail.com>
2025-04-14 15:18:20 +0800

    prep

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hlBlue: "#0d6efd",
+        hlIndigo: "#6610f2",
+        hlPurple: "#6f42c1",
+        hlPink: "#d63384",
+        hlRed: "#dc3545",
+        hlOrange: "#fd7e14",
+        hlYellow: "#ffc107",
+        hlGreen: "#198754",
+        hlTeal: "#20c997",
+        hlCyan: "#0dcaf0",
+        hlWhite: "#fff",
+        hlGray: "#6c757d",
+        hlGrayDark: "#343a40",
+        hlPrimary: "#0d6efd",
+        hlSecondary: "#6c757d",
+        hlSuccess: "#198754",
+        hlInfo: "#0dcaf0",
+        hlWarning: "#ffc107",
+        hlDanger: "#dc3545",
+        hlLight: "#f8f9fa",
+        hlDark: "#212529",
+      },
+    },
   },
   plugins: [],
 }
